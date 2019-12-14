62c8da8cb4f758aa37a99f13df440ea713297f3b
Kafin Salim <kafinsalim@gmail.com>
2019-12-14 18:45:06 +0700

    fix: remove missing import on deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import * as React from "react";
 import { StoreProvider, createStore } from "easy-peasy";
 import AppRoute from "./commons/AppRoute";
 import model from "./model";
-import "antd/dist/antd.css";
 import "antd-mobile/dist/antd-mobile.css";
 
 const store = createStore(model);
